614eac5c570b7e7d7ca69d020cd557a0f6018728
Olha Pylypova <olhasve@gmail.com>
2021-05-20 15:18:48 +0300

    first commit

---
+++ src/Redux/store.js
@@ -0,0 +1,45 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import contactsReducer from "./Phone/phone-reducer";
+import  authReducer from "./auth/auth_reducer"
+
+const authConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+// const rootReducer = combineReducers({ contacts: contactsReducer });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: {
+        auth: persistReducer(authConfig, authReducer),
+        contacts: contactsReducer
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
+});
+
+const persistor = persistStore(store);;
+export default { store, persistor };
+// export default store;
